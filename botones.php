4257305f840a0caaf0570b211d70befb16bf66af
Andres Torres <jatobrun@espol.edu.ec>
2021-01-13 13:22:06 -0500

    Cree los archivos correspondientes a la practica 12 de sistemas embebidos

---
+++ botones.php
@@ -0,0 +1,74 @@
+<html>
+<head>
+<meta name="viewport" content="width=device-width" />
+<title>PRÁCTICA 12 SISTEMAS EMBEBIDOS.</title>
+</head>
+       <body>
+       <center><h1>Control web SISTEMAS EMBEBIDOS 2020 IIT</h1>
+         <form method="get" action="botones.php">
+            <input type="submit" style = "font-size: 16 pt" value="ApagarPatio" name="offPatio">
+            <input type="submit" style = "font-size: 16 pt" value="EncenderPatio" name="onPatio">
+         </form></center>
+
+	<center><form method="get" action="botones.php">
+         <input type="submit" style = "font-size: 16 pt" value="ApagarParqueo" name="offParqueo">
+            <input type="submit" style = "font-size: 16 pt" value="EncenderParqueo" name="onParqueo">
+         </form></center>
+
+	<center><form method="get" action="botones.php">
+         <input type="submit" style = "font-size: 16 pt" value="ApagarSala" name="offSala">
+            <input type="submit" style = "font-size: 16 pt" value="EncenderSala" name="onSala">
+         </form></center>
+
+	<center><form method="get" action="botones.php">
+         <input type="submit" style = "font-size: 16 pt" value="ApagarEntrada" name="offEntrada">
+            <input type="submit" style = "font-size: 16 pt" value="EncenderEntrada" name="onEntrada">
+         </form></center>
+
+<?php
+     if(isset($_GET['offPatio']))
+        {
+                       echo "Patio apagado";
+                        system ("sudo python3 patiooff.py");
+        }
+            else if(isset($_GET['onPatio']))
+            {
+                        echo "Patio encendido";
+                        exec("sudo python3 patioon.py");
+            }
+            else if(isset($_GET['onParqueo']))
+            {
+                        echo "Parqueo encendido";
+                        exec("sudo python3 garajeon.py");
+            }
+            else if(isset($_GET['offParqueo']))
+            {
+                        echo "Parqueo apagado";
+                        exec("sudo python3 garajeoff.py");
+            }
+            else if(isset($_GET['onSala']))
+            {
+                        echo "Sala encendida";
+                        exec("sudo python3 salaon.py");
+            }
+            else if(isset($_GET['offSala']))
+            {
+                        echo "Sala Apagada";
+                        exec("sudo python3 salaoff.py");
+            }
+            else if(isset($_GET['onEntrada']))
+            {
+                        echo "Entrada encendida";
+                        exec("sudo python3 entradaon.py");
+            }
+            else if(isset($_GET['offEntrada']))
+            {
+                        echo "Entrada apagada";
+                        exec("sudo python3 entradaoff.py");
+            }
+	else{
+			echo "SELECCIONE ACCIÓN";
+	    }
+?>
+   </body>
+</html>	
